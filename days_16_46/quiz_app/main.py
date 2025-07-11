f030b3c757021d1999b9e83a7f3d911474a1344d
Kemi Thomas <kemothom71@gmail.com>
2025-07-11 17:16:29 -0400

    Update main.py

--- days_16_46/quiz_app/main.py
+++ days_16_46/quiz_app/main.py
@@ -1,11 +1,21 @@
-import requests
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from ui import QuizInterface
 
-parameters = {
-    "amount": 10,
-    "type": "boolean",
-    "category": 18
-}
-response = requests.get("https://opentdb.com/api.php", params=parameters)
-response.raise_for_status()
-data = response.json()
-question_data = data['results']
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
